eb3e7f8833db9c8ebebdfe0976734132603a52a6
Lance Easley <atlasle52671015@gmail.com>
2021-07-27 13:35:03 -0500

    Commented and improved test code

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
 import math
 
 def getDigit(num, index):
+    # Get number of digits in num
     digits = math.ceil(math.log10(num))
 
+    # Throw -1 if index is out of bounds
     if (index > digits):
         return -1
 
-    place = abs(index + 1 - digits)
+    # Invert index to number of places to divide off
+    times_to_divide = abs(index + 1 - digits)
 
-    return (num // 10 ** place) % 10
+    # Divide and return
+    return (num // 10 ** times_to_divide) % 10
 
+# Trial code
 while True:
     numInput = int(input("num: "))
     indexInput = int(input("index: "))
     print(getDigit(numInput, indexInput))
 
+# Test code
 def test():
     test1 = getDigit(12345, 2)
     test2 = getDigit(54321, 0)
     test3 = getDigit(349857345, 5)
     test4 = getDigit(10, 5)
-    if (test1 == 3):
-        print("pass")
-    else: 
-        print(test1)
-    if (test2 == 5):
-        print("pass")
-    else: 
-        print(test2)
-    if (test3 == 7):
-        print("pass")
-    else: 
-        print(test3)
-    if (test4 == -1):
-        print("pass")
-    else: 
-        print(getDigit(10, 5) )
+    assert test1 == 3
+    assert test2 == 5
+    assert test3 == 7
+    assert test4 == -1
 
 test()
